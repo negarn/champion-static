3086ffea38d0e019127996a8ae1d3da9146db8e4
negar <negar@binary.com>
2017-01-10 09:59:11 +0800

    Auto-generated commit

--- js/bundle.js
+++ js/bundle.js
@@ -18463,10 +18463,9 @@
 	// const ChampionNewReal    = require('./../pages/new_account/real');
 	var ChampionContact = __webpack_require__(299);
 	var ChampionEndpoint = __webpack_require__(313);
-	var BinaryOptions = __webpack_require__(314);
 	var Client = __webpack_require__(304);
-	var LoggedIn = __webpack_require__(316);
-	var Login = __webpack_require__(317);
+	var LoggedIn = __webpack_require__(314);
+	var Login = __webpack_require__(315);
 
 	var Champion = function () {
 	    'use strict';
@@ -18480,9 +18479,9 @@
 	        _signup = $('#signup');
 	        _container.on('champion:before', beforeContentChange);
 	        _container.on('champion:after', afterContentChange);
-	        Client.init();
 	        ChampionRouter.init(_container, '#champion-content');
 	        ChampionSocket.init();
+	        Client.init();
 	        if (!Client.is_logged_in()) {
 	            $('#main-login a').on('click', function () {
 	                Login.redirect_to_login();
@@ -18503,21 +18502,18 @@
 	        var page = content.getAttribute('data-page');
 	        var pages_map = {
 	            virtual: ChampionNewVirtual,
-	            // real            : ChampionNewReal,
+	            // real       : ChampionNewReal,
 	            contact: ChampionContact,
 	            endpoint: ChampionEndpoint,
-	            logged_inws: LoggedIn,
-	            'binary-options': BinaryOptions
+	            logged_inws: LoggedIn
 	        };
 	        if (page in pages_map) {
 	            _active_script = pages_map[page];
 	            _active_script.load();
 	        }
 
-	        var form = _container.find('#verify-email-form');
-	        if (Client.is_logged_in() || /new-account/.test(window.location.pathname)) {
-	            form.hide();
-	        } else {
+	        if (!Client.is_logged_in()) {
+	            var form = _container.find('#verify-email-form');
 	            if (!_active_script) _active_script = ChampionSignup;
 	            ChampionSignup.load(form.length ? form : _signup);
 	        }
@@ -18930,11 +18926,9 @@
 	        return string.split('+').sort().map(function (str) {
 	            var items = str.split(':');
 	            var id = items[0];
-	            var real = items[1] === 'R';
-	            if (real) client_object.has_real = real;
 	            return {
 	                id: id,
-	                real: real,
+	                real: items[1] === 'R',
 	                disabled: items[2] === 'D'
 	            };
 	        });
@@ -18971,7 +18965,7 @@
 
 	    // use this function to get variables that are a boolean
 	    var get_boolean = function get_boolean(value) {
-	        return JSON.parse(client_object[value] || get_storage_value(value) || false);
+	        return JSON.parse(get_storage_value(value) || false);
 	    };
 
 	    var response_authorize = function response_authorize(response) {
@@ -19085,7 +19079,6 @@
 	        redirect_if_login: redirect_if_login,
 	        set_value: set_storage_value,
 	        get_value: get_storage_value,
-	        get_boolean: get_boolean,
 	        response_authorize: response_authorize,
 	        clear_storage_values: clear_storage_values,
 	        get_token: get_token,
@@ -19774,7 +19767,7 @@
 	                type: 'account_opening'
 	            }, function (response) {
 	                if (response.verify_email) {
-	                    ChampionRouter.forward(url_for('new-account/virtual'));
+	                    ChampionRouter.forward(url_for('create-account'));
 	                }
 	            });
 	        }
@@ -20026,259 +20019,6 @@
 
 	'use strict';
 
-	__webpack_require__(315);
-	var Client = __webpack_require__(304);
-
-	var BinaryOptions = function () {
-	    'use strict';
-
-	    var load = function load() {
-	        if (Client.is_logged_in()) {
-	            $('#virtual-signup-button').hide();
-	            if (Client.get_boolean('has_real')) {
-	                $('#real-signup-button').hide();
-	            }
-	        } else {
-	            $('#virtual-signup-button').on('click', function () {
-	                $.scrollTo('#verify-email-form', 500);
-	            });
-	        }
-	    };
-
-	    var unload = function unload() {
-	        $('#virtual-signup-button').off('click');
-	    };
-
-	    return {
-	        load: load,
-	        unload: unload
-	    };
-	}();
-
-	module.exports = BinaryOptions;
-
-/***/ },
-/* 315 */
-/***/ function(module, exports, __webpack_require__) {
-
-	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
-	 * jQuery.scrollTo
-	 * Copyright (c) 2007-2015 Ariel Flesler - aflesler<a>gmail<d>com | http://flesler.blogspot.com
-	 * Licensed under MIT
-	 * http://flesler.blogspot.com/2007/10/jqueryscrollto.html
-	 * @projectDescription Lightweight, cross-browser and highly customizable animated scrolling with jQuery
-	 * @author Ariel Flesler
-	 * @version 2.1.2
-	 */
-	;(function(factory) {
-		'use strict';
-		if (true) {
-			// AMD
-			!(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__(1)], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
-		} else if (typeof module !== 'undefined' && module.exports) {
-			// CommonJS
-			module.exports = factory(require('jquery'));
-		} else {
-			// Global
-			factory(jQuery);
-		}
-	})(function($) {
-		'use strict';
-
-		var $scrollTo = $.scrollTo = function(target, duration, settings) {
-			return $(window).scrollTo(target, duration, settings);
-		};
-
-		$scrollTo.defaults = {
-			axis:'xy',
-			duration: 0,
-			limit:true
-		};
-
-		function isWin(elem) {
-			return !elem.nodeName ||
-				$.inArray(elem.nodeName.toLowerCase(), ['iframe','#document','html','body']) !== -1;
-		}		
-
-		$.fn.scrollTo = function(target, duration, settings) {
-			if (typeof duration === 'object') {
-				settings = duration;
-				duration = 0;
-			}
-			if (typeof settings === 'function') {
-				settings = { onAfter:settings };
-			}
-			if (target === 'max') {
-				target = 9e9;
-			}
-
-			settings = $.extend({}, $scrollTo.defaults, settings);
-			// Speed is still recognized for backwards compatibility
-			duration = duration || settings.duration;
-			// Make sure the settings are given right
-			var queue = settings.queue && settings.axis.length > 1;
-			if (queue) {
-				// Let's keep the overall duration
-				duration /= 2;
-			}
-			settings.offset = both(settings.offset);
-			settings.over = both(settings.over);
-
-			return this.each(function() {
-				// Null target yields nothing, just like jQuery does
-				if (target === null) return;
-
-				var win = isWin(this),
-					elem = win ? this.contentWindow || window : this,
-					$elem = $(elem),
-					targ = target, 
-					attr = {},
-					toff;
-
-				switch (typeof targ) {
-					// A number will pass the regex
-					case 'number':
-					case 'string':
-						if (/^([+-]=?)?\d+(\.\d+)?(px|%)?$/.test(targ)) {
-							targ = both(targ);
-							// We are done
-							break;
-						}
-						// Relative/Absolute selector
-						targ = win ? $(targ) : $(targ, elem);
-						/* falls through */
-					case 'object':
-						if (targ.length === 0) return;
-						// DOMElement / jQuery
-						if (targ.is || targ.style) {
-							// Get the real position of the target
-							toff = (targ = $(targ)).offset();
-						}
-				}
-
-				var offset = $.isFunction(settings.offset) && settings.offset(elem, targ) || settings.offset;
-
-				$.each(settings.axis.split(''), function(i, axis) {
-					var Pos	= axis === 'x' ? 'Left' : 'Top',
-						pos = Pos.toLowerCase(),
-						key = 'scroll' + Pos,
-						prev = $elem[key](),
-						max = $scrollTo.max(elem, axis);
-
-					if (toff) {// jQuery / DOMElement
-						attr[key] = toff[pos] + (win ? 0 : prev - $elem.offset()[pos]);
-
-						// If it's a dom element, reduce the margin
-						if (settings.margin) {
-							attr[key] -= parseInt(targ.css('margin'+Pos), 10) || 0;
-							attr[key] -= parseInt(targ.css('border'+Pos+'Width'), 10) || 0;
-						}
-
-						attr[key] += offset[pos] || 0;
-
-						if (settings.over[pos]) {
-							// Scroll to a fraction of its width/height
-							attr[key] += targ[axis === 'x'?'width':'height']() * settings.over[pos];
-						}
-					} else {
-						var val = targ[pos];
-						// Handle percentage values
-						attr[key] = val.slice && val.slice(-1) === '%' ?
-							parseFloat(val) / 100 * max
-							: val;
-					}
-
-					// Number or 'number'
-					if (settings.limit && /^\d+$/.test(attr[key])) {
-						// Check the limits
-						attr[key] = attr[key] <= 0 ? 0 : Math.min(attr[key], max);
-					}
-
-					// Don't waste time animating, if there's no need.
-					if (!i && settings.axis.length > 1) {
-						if (prev === attr[key]) {
-							// No animation needed
-							attr = {};
-						} else if (queue) {
-							// Intermediate animation
-							animate(settings.onAfterFirst);
-							// Don't animate this axis again in the next iteration.
-							attr = {};
-						}
-					}
-				});
-
-				animate(settings.onAfter);
-
-				function animate(callback) {
-					var opts = $.extend({}, settings, {
-						// The queue setting conflicts with animate()
-						// Force it to always be true
-						queue: true,
-						duration: duration,
-						complete: callback && function() {
-							callback.call(elem, targ, settings);
-						}
-					});
-					$elem.animate(attr, opts);
-				}
-			});
-		};
-
-		// Max scrolling position, works on quirks mode
-		// It only fails (not too badly) on IE, quirks mode.
-		$scrollTo.max = function(elem, axis) {
-			var Dim = axis === 'x' ? 'Width' : 'Height',
-				scroll = 'scroll'+Dim;
-
-			if (!isWin(elem))
-				return elem[scroll] - $(elem)[Dim.toLowerCase()]();
-
-			var size = 'client' + Dim,
-				doc = elem.ownerDocument || elem.document,
-				html = doc.documentElement,
-				body = doc.body;
-
-			return Math.max(html[scroll], body[scroll]) - Math.min(html[size], body[size]);
-		};
-
-		function both(val) {
-			return $.isFunction(val) || $.isPlainObject(val) ? val : { top:val, left:val };
-		}
-
-		// Add special hooks so that window scroll properties can be animated
-		$.Tween.propHooks.scrollLeft = 
-		$.Tween.propHooks.scrollTop = {
-			get: function(t) {
-				return $(t.elem)[t.prop]();
-			},
-			set: function(t) {
-				var curr = this.get(t);
-				// If interrupt is true and user scrolled, stop animating
-				if (t.options.interrupt && t._last && t._last !== curr) {
-					return $(t.elem).stop();
-				}
-				var next = Math.round(t.now);
-				// Don't waste CPU
-				// Browsers don't render floating point scroll
-				if (curr !== next) {
-					$(t.elem)[t.prop](next);
-					t._last = this.get(t);
-				}
-			}
-		};
-
-		// AMD requirement
-		return $scrollTo;
-	});
-
-
-/***/ },
-/* 316 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-
 	var isEmptyObject = __webpack_require__(308).isEmptyObject;
 	var getLanguage = __webpack_require__(303).getLanguage;
 	var Client = __webpack_require__(304);
@@ -20365,7 +20105,7 @@
 	module.exports = LoggedIn;
 
 /***/ },
-/* 317 */
+/* 315 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
