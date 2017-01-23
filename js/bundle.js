5c80befe29ec1d97d4c9ec150be03bf7dfe124e9
negar <negar@binary.com>
2017-01-23 17:46:57 +0800

    Auto-generated commit

--- js/bundle.js
+++ js/bundle.js
@@ -19630,9 +19630,9 @@
 	                tag_to_find = 'a';
 	            }
 	            /* eslint-disable newline-per-chained-call */
-	            var $parent_el = $(menu).find('li').removeClass(parent_active).find(tag_to_find).removeClass(child_active);
+	            var $parent_el = $(menu).find('li').removeClass(parent_active).find(tag_to_find).removeClass(child_active).end().find(hash);
 	            if (has_subtab) {
-	                $parent_el = $parent_el.end().end().find(hash).find('.tm-ul-2').find(tag_to_find).first().addClass(child_active).end().end().end();
+	                $parent_el = $parent_el.find('.tm-ul-2').find(tag_to_find).first().addClass(child_active).end().end().end();
 	            }
 	            $parent_el.addClass(parent_active);
 
