3bc6b80e6b81c35c13c9ef17c42fdab3fa9489ba
negar <negar@binary.com>
2017-01-25 09:46:20 +0800

    Auto-generated commit

--- js/bundle.js
+++ js/bundle.js
@@ -19625,12 +19625,9 @@
 	            $(menu).find('li').removeClass(parent_active).find('span').removeClass(child_active);
 
 	            var $tab_to_show = $(hash);
-	            var has_subtab = $tab_to_show.find('.tm-li-2').length > 0;
-	            if (has_subtab || /tm-li-2/.test($(hash).attr('class'))) {
-	                if (has_subtab) {
-	                    $tab_to_show = $tab_to_show.find('.tm-ul-2');
-	                }
-	                $tab_to_show = $tab_to_show.find('span').first().addClass(child_active).closest('li.tm-li');
+	            // if hash is a subtab or has subtabs
+	            if ($tab_to_show.find('.tm-li-2').length > 0 || /tm-li-2/.test($(hash).attr('class'))) {
+	                $tab_to_show = $tab_to_show.find('span.tm-a-2').first().addClass(child_active).closest('li.tm-li');
 	            }
 	            $tab_to_show.addClass(parent_active);
 
